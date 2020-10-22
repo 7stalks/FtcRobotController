6c140b5b3c9068162c42f08735e39049d223ed78
Connor Olson <condolson@gmail.com>
2020-10-21 19:03:17 -0700

    Commit code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usercode/Connor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usercode/Connor.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode.usercode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Connor Servo move", group = "Connor")
 public class Connor extends LinearOpMode {
     Servo servo;
+    DigitalChannel sensor;
 
     double counter = 0.01;
     double max_pos = 1.0;
@@ -19,18 +21,21 @@ public class Connor extends LinearOpMode {
         int foo = 0;
         servo = hardwareMap.get(Servo.class, "servo");
 
+        sensor = hardwareMap.get(DigitalChannel.class, "sensor");
+        sensor.setMode(DigitalChannel.Mode.INPUT);
+
         telemetry.addData(">", "Press Start to scan Servo.");
         telemetry.update();
         waitForStart();
 
         while(opModeIsActive()){
+            if (sensor.getState() == true) {
 
-            if (gamepad1.left_trigger>0){
-                // left trigger goes from 0 > 1
-                // at zero hold position
-                // stick at 1/2 servo == .5
-                // a
+            } else {
+                
             }
+
+
             if (gamepad1.left_trigger > 0){
                 counter += 0.01;
                 if (counter >= max_pos){
@@ -43,6 +48,7 @@ public class Connor extends LinearOpMode {
                 }
             }
             servo.setPosition(counter);
+
             telemetry.addData("Running", counter);
             telemetry.update();
 
