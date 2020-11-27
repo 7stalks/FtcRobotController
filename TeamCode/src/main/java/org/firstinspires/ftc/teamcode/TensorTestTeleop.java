0dd4220e2e16dd18879ee6373c5a92ba72a5e298
Ryan Swart <rswart0604@gmail.com>
2020-11-27 14:03:10 -0700

    need to try to test the tumultuous tensor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorTestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorTestTeleop.java
@@ -4,20 +4,24 @@ import android.graphics.Path;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 import java.util.List;
 
-@TeleOp(name = "Vuforia Test")
-public class VuforiaTestTeleop extends LinearOpMode {
+@TeleOp(name = "Tensor Test")
+public class TensorTestTeleop extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
     GoBildaDrive drive = new GoBildaDrive(robot);
+    ElapsedTime timer = new ElapsedTime();
 
-    public void checkForRings() {
+    public String checkForRings(int seconds) {
+        String numberOfRings = "";
         List<Recognition> updatedRecognitions = robot.tensorFlowEngine.getUpdatedRecognitions();
-        while (true) {
+        timer.reset();
+        while (numberOfRings.equals("") && timer.seconds() < seconds) {
             telemetry.addData("# Object Detected", updatedRecognitions.size());
             // step through the list of recognitions and display boundary info.
             int i = 0;
@@ -29,12 +33,13 @@ public class VuforiaTestTeleop extends LinearOpMode {
                         recognition.getRight(), recognition.getBottom());
                 if (recognition.getLabel().equals("Quad") || recognition.getLabel().equals("Single")) {
                     telemetry.addLine(String.format("I found a %s", recognition.getLabel()));
-
+                    numberOfRings = recognition.getLabel();
+                    break;
                 }
             }
             telemetry.update();
-
         }
+        return numberOfRings;
     }
 
     @Override
@@ -46,20 +51,8 @@ public class VuforiaTestTeleop extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            List<Recognition> updatedRecognitions = robot.tensorFlowEngine.getUpdatedRecognitions();
-            if (updatedRecognitions != null) {
-                telemetry.addData("# Object Detected", updatedRecognitions.size());
-                // step through the list of recognitions and display boundary info.
-                int i = 0;
-                for (Recognition recognition : updatedRecognitions) {
-                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                            recognition.getLeft(), recognition.getTop());
-                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                            recognition.getRight(), recognition.getBottom());
-                }
-                telemetry.update();
-            }
+            String numberOfRings = checkForRings(5);
+            telemetry.addData("Number of rings", numberOfRings);
         }
     }
 }
